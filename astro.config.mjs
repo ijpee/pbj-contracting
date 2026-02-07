49a968f064eb28551e056282fa5d85101d2a7d15
Isaiah Poupouare <Isaiah.Poupouare@kindo.co.nz>
2026-02-07 13:55:17 +1300

    Update site content and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
+  prefetch: true,
+  site: 'https://pbjcontracting.co.nz', 
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
